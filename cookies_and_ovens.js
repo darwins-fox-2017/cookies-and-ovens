681b47a50b25bc68b3da9d4d7637779b977c5da6
Bambang Handoko <email": null>
2017-02-07 14:04:18 +0700

    Cookies and Oven Done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,59 @@
 //
 //
 // Your code here
+class Cookies {
+  constructor() {
+    this.status = 'Mentah'
+  }
+}
+
+class PeanutButter extends Cookies{
+  constructor() {
+    super()
+    this.cookieName = "Peanut Butter"
+    this.cookedTime = 45
+  }
+}
+
+class ChocolateChip extends Cookies{
+  constructor() {
+    super()
+    this.cookieName = "Chocolate Chip"
+    this.cookedTime = 20
+  }
+}
+
+class Oven {
+  constructor() {
+    this._tray = []
+  }
+
+  addCookies(cookie) {
+    this._tray.push(cookie)
+  }
+
+  startBake(cookingTime) {
+    for (let i = 0; i <= cookingTime; i++) {
+      for (let j = 0; j < this._tray.length; j++) {
+        if (i > Math.round(this._tray[j].cookedTime)) {
+          console.log(`Status Cookies dalam menit ke-${i} menit, ${this._tray[j].cookieName} gosong`)
+        } else if (i === this._tray[j].cookedTime) {
+          console.log(`Status Cookies dalam menit ke-${i} menit, ${this._tray[j].cookieName} matang`)
+        } else if (i >= this._tray[j].cookedTime/2) {
+          console.log(`Status Cookies dalam menit ke-${i} menit, ${this._tray[j].cookieName} hampir matang`)
+        } else if (i <= this._tray[j].cookedTime/4){
+          console.log(`Status Cookies dalam menit ke-${i} menit, ${this._tray[j].cookieName} mentah`)
+        }
+      }
+      console.log('----------------------------------------------------------------------');
+    }
+  }
+}
+
+let peanutButter = new PeanutButter()
+let chocolateChip = new ChocolateChip()
+let oven = new Oven()
+
+oven.addCookies(peanutButter)
+oven.addCookies(chocolateChip)
+oven.startBake(45)
