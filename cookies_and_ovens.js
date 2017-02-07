a5de1c28370ee0d21e0f06635ec1e14074270c4a
Eri Irawan <eryirawan91@gmail.com>
2017-02-07 11:23:41 +0700

    all release is done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -15,5 +15,51 @@ class Cookies {
     this.timeMature = 0
     this.status     = "Mentah"
   }
-  
+
+  cookCondition(){
+    this.timeCook++
+    if (this.timeCook <= 5) {return this.status }
+    if (this.timeCook <= 10) {return this.status = "Hampir matang!"}
+    if (this.timeCook == this.timeMature) {return this.status = "Matang!"}
+    if (this.timeCook > this.timeMature) {return this.status = "Gosong!"}
+  }
 }
+
+class PeanutCake extends Cookies {
+  constructor(setTime, timeMature) {
+    super(setTime)
+    this.timeMature = 13
+  }
+}
+
+class ChocolateCake extends Cookies {
+  constructor(setTime, timeMature) {
+    super(setTime)
+    this.timeMature = 12
+  }
+}
+
+let cookiesResult = new PeanutCake(3)
+    cookiesResult.cookCondition()
+
+console.log(cookiesResult);
+cookiesResult.cookCondition()
+console.log(cookiesResult);
+cookiesResult.cookCondition()
+console.log(cookiesResult);
+cookiesResult.cookCondition()
+console.log(cookiesResult);
+cookiesResult.cookCondition()
+console.log(cookiesResult);
+cookiesResult.cookCondition()
+console.log(cookiesResult);
+cookiesResult.cookCondition()
+console.log(cookiesResult);
+cookiesResult.cookCondition()
+console.log(cookiesResult);
+cookiesResult.cookCondition()
+console.log(cookiesResult);
+cookiesResult.cookCondition()
+console.log(cookiesResult);
+cookiesResult.cookCondition()
+console.log(cookiesResult);
