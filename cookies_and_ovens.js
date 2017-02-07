cb571b9d6dada1436239ff7d0e5081df9f2eb0f8
mfpebrizal <ftm.pebrizal@gmail.com>
2017-02-07 13:21:47 +0700

    Done all release

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,10 +1,71 @@
-// Answer These Questions:
-//
-// - What are essential classes?
-// - What attributes will each class have?
-// - What interface will each class provide?
-// - How will the classes interact with each other?
-// - Which classes will inherit from others, if any?
-//
-//
-// Your code here
+class Cookies{
+  constructor(){
+    this.status = 'raw'
+  }
+}
+
+class Oven{
+  constructor(){
+    this._tray = [];
+  }
+
+  addCookies(cookies) {
+    this._tray.push(cookies)
+  }
+
+  bake(time){
+    console.log(time);
+
+    for(let i=0; i<=time; i+=2){
+      for(let j=0; j<this._tray.length; j++){
+        if(i < this._tray[j].cooked_time/2+5){
+          this._tray[j].status = this._tray[j].status
+        }else if(i< this._tray[j].cooked_time-5){
+          this._tray[j].status = "almost done"
+        }else if(i == this._tray[j].cooked_time){
+          this._tray[j].status = "done"
+        }else {
+          this._tray[j].status = "burned"
+        }
+        console.log(`minute ${i} : your ${this._tray[j].name} cookies is ${this._tray[j].status}`);
+      }
+      console.log('=========================================================');
+    }
+  }
+}
+
+
+class PeanutButterCookies extends Cookies{
+  constructor(){
+    super()
+    this.name = "Peanut Butter"
+    this.cooked_time = 60
+  }
+}
+
+class ChocolateCookies extends Cookies{
+  constructor(){
+    super()
+    this.name = "Chocolate Chips"
+    this.cooked_time = 40
+  }
+}
+
+class PlainCookies extends Cookies{
+  constructor(){
+    super()
+    this.name = "Plain"
+    this.cooked_time = 30
+  }
+}
+
+let pbCookies = new PeanutButterCookies()
+let chocoCookies = new ChocolateCookies()
+let plaincookies = new PlainCookies()
+
+let oven = new Oven(pbCookies)
+
+oven.addCookies(pbCookies)
+oven.addCookies(chocoCookies)
+oven.addCookies(plaincookies)
+oven.bake(60)
