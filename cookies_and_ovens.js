146af98acf174701afe5b3a2fb6a0c5fb29eb45a
adamsaparudin <adamsaparudin@gmail.com>
2017-02-07 10:53:55 +0700

    SELESAI

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,65 @@
 //
 //
 // Your code here
+
+class Cookies {
+  constructor() {
+    this.status = "MENTAH"
+    this.time = 0
+    this.MATANG_TIME = 30
+  }
+}
+
+class ChoclateCookies extends Cookies {
+  constructor(status) {
+    super(status)
+    this.name = "Kue Coklat"
+    this.MATANG_TIME = 30
+  }
+}
+
+class PeanutCookies extends Cookies {
+  constructor(status) {
+    super(status)
+    this.name = "Kue Kacang"
+    this.MATANG_TIME = 40
+  }
+}
+
+class Ovens {
+  constructor(isiOven) {
+    this.isiOven = isiOven
+  }
+  bake(n) {
+    for(let j=1; j<=n; j++) {
+      batch1.status()
+      console.log("-------- STATUS MASAK MENIT KE-" + j*10 + "---------")
+      for(let i=0; i<this.isiOven.length; i++) {
+        this.isiOven[i].time += 10
+
+        if(this.isiOven[i].time > this.isiOven[i].MATANG_TIME)
+          this.isiOven[i].status = "GOSONG"
+        else if(this.isiOven[i].time == this.isiOven[i].MATANG_TIME)
+          this.isiOven[i].status = "MATANG"
+        else
+          this.isiOven[i].status = "MENTAH"
+      }
+    }
+  }
+  status() {
+    for(let i=0; i<this.isiOven.length; i++) {
+      console.log(`${this.isiOven[i].name}, Sudah : ${this.isiOven[i].status}`)
+    }
+  }
+}
+
+
+let a = new PeanutCookies()
+let b = new PeanutCookies()
+let c = new ChoclateCookies()
+
+let batch1 = new Ovens([a, b, c])
+
+batch1.bake(4)
+
+batch1.status()
